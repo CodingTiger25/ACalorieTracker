a29bf0ffd58aea2e32e9f0ce3ca8be32af0bca76
Tony Padmore <emperialace@gmail.com>
2023-04-18 20:09:05 -0400

    'Added other meals for the day'

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('shows track your calories', () => {
+  render(<App></App>);
+
+  //Assert
+  const trackCalorieElement = screen.getByText('Track your calories', {exact: false});
+  expect(trackCalorieElement).toBeInTheDocument();
+});
+
+test('shows CALORIES ', () => {
+  render(<App></App>);
+
+  //Assert
+  const trackCalorieElement = screen.getByText('CALORIES');
+  expect(trackCalorieElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 //import {reactLocalStorage} from 'reactjs-localstorage';
 import './App.css';
 import styled from 'styled-components';
-
+import {v4 as uuidv4} from 'uuid';
 const Title = styled.section`
   color:black;
   display: block;
@@ -32,7 +32,6 @@ const CalorieButton = styled.button`
 
  `;
 
-
 const Calories = styled.p`
   font-size: 5rem;
   color: black;
@@ -45,32 +44,42 @@ const AddItems = styled.button`
 
 `;
 
-/*const ListItems = 
-styled.input.attrs(props:any => ({
-  type: "text",
-
-  size: props.size || "15rem",
-
-}))` 
-  
-  color: palevioletred;
-  font-size: 1em;
-  border: 5px solid palevioletred;
-`;*/
-
 const Background = styled.div`
 
   background-color: #6868e7;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
+  padding: 5px;
 `;
 
+const Line = styled.div`
+  
+  height: 3px;
+  width: 100%;
+  background: black;
+`
 function App() {
  
   let [currCalories, setCurrCalories] = useState(0);
 
-  const [foodItem, setFoodItem] = useState({name:"",calories:0});
-  const [foodCalorie, setFoodCalorie] = useState([]);
+  let [breakfastItem, setBreakfastItem] = useState([{ id: '', name:'',calories:0}]);
+  let [lunchItem, setLunchItem] = useState([{ id: '', name:'',calories:0}]);
+  let [dinnerItem, setDinnerItem] = useState([{ id: '', name:'',calories:0}]);
+  let [snackItem, setSnackItem] = useState([{ id: '', name:'',calories:0}]);
+
+  let [foodName, setFoodName] = useState('');
+  let [foodCalorie, setFoodCalorie] = useState(0);
+  let [usedCalorie, setUsedCalorie] = useState(0);
+  let [initCalorie, setInitCalorie] = useState(0);
+
+  const[showForm, setShowForm] = useState(false);
+  let [foodTime, setFoodTime] = useState("");
+
+  const [breakfast, setBreakfast] = useState(false);
+  const [lunch, setLunch] = useState(false);
+  const [dinner, setDinner] = useState(false);
+  const [snack, setSnack] = useState(false);
+
 
   const inputCal = () => {
 
@@ -84,73 +93,431 @@ function App() {
         cal = parseInt(calories);
         
     }
+    localStorage.setItem('InitCalorie',JSON.stringify(cal));
     setCurrCalories(cal);
+    setInitCalorie(cal);
+}
+const changeCategory = (e:String) => {
+
+    if(e.localeCompare("b") === 0)
+    {
+      setFoodTime("b");
+    }
+    else if(e.localeCompare("l") === 0)
+    {
+      setFoodTime("l");
+    }
+    else if(e.localeCompare("d") === 0)
+    {
+      setFoodTime("d");
+    }
+    else if(e.localeCompare("s") === 0)
+    {
+      setFoodTime("s");
+    }
 }
 
-/*const inputFood = (event) => {
+  let handleSubmit = (e:any) => {
 
-    setFoodItem({...foodItem,
-      [event.target.name]: event.target.value});
+    e.preventDefault();
+
+
+    if(currCalories === 0)
+    {
+      alert('Set calorie limit!');
+    }
+
+    else{
+
+          if(foodTime.localeCompare("b") === 0)
+          {
+            let calo = currCalories - foodCalorie;
+            let usedCalo = foodCalorie + usedCalorie;
+            setBreakfastItem([{id: uuidv4() , name:foodName,
+              calories:foodCalorie},...breakfastItem
+            ])
+            setCurrCalories(calo);
+            setUsedCalorie(usedCalo);
+            localStorage.setItem('usedUpCalorie', JSON.stringify(usedCalo)
+            )
+    
+            console.log('used Calorie type: ', typeof(usedCalorie));
+            console.log('food Calorie type: ', typeof(foodCalorie));
+    
+            
+            //let calo = currCalories;
+          }
+          else if(foodTime.localeCompare("l") === 0)
+          {
+            let calo = currCalories - foodCalorie;
+            let usedCalo = foodCalorie + usedCalorie;
+            setLunchItem([{id: uuidv4() , name:foodName,
+              calories:foodCalorie},...lunchItem
+            ])
+            setCurrCalories(calo);
+            setUsedCalorie(usedCalo);
+            localStorage.setItem('usedUpCalorie', JSON.stringify(usedCalo)
+            )
+    
+            console.log('used Calorie type: ', typeof(usedCalorie));
+            console.log('food Calorie type: ', typeof(foodCalorie));
+            }
+            else if(foodTime.localeCompare("d") === 0)
+          {
+            let calo = currCalories - foodCalorie;
+            let usedCalo = foodCalorie + usedCalorie;
+            setDinnerItem([{id: uuidv4() , name:foodName,
+              calories:foodCalorie},...dinnerItem
+            ])
+            setCurrCalories(calo);
+            setUsedCalorie(usedCalo);
+            localStorage.setItem('usedUpCalorie', JSON.stringify(usedCalo)
+            )
+    
+            console.log('used Calorie type: ', typeof(usedCalorie));
+            console.log('food Calorie type: ', typeof(foodCalorie));
+            }
+            else if(foodTime.localeCompare("s") === 0)
+          {
+            let calo = currCalories - foodCalorie;
+            let usedCalo = foodCalorie + usedCalorie;
+            setSnackItem([{id: uuidv4() , name:foodName,
+              calories:foodCalorie},...snackItem
+            ])
+            setCurrCalories(calo);
+            setUsedCalorie(usedCalo);
+            localStorage.setItem('usedUpCalorie', JSON.stringify(usedCalo)
+            )
+    
+            console.log('used Calorie type: ', typeof(usedCalorie));
+            console.log('food Calorie type: ', typeof(foodCalorie));
+            }
+          }
+        }
+   
+  function displayBreakfastItems(List:any) {
+      return breakfastItem.map((foods)=> (
+    
+        <div>
+          <h1>{foods.name}  {foods.calories} <button onClick={() => {remove(foods.id,"b")
+
+          }}>Remove</button>
+              {/* <button onClick={() => remove(foods.id)}>X</button> */}
+          </h1>
+        </div>
+      ))
+  } 
+
+  function displayLunchItems(List:any) {
+    return lunchItem.map((lunchFoods)=> (
   
-};*/
+      <div>
+        <h1>{lunchFoods.name}  {lunchFoods.calories} <button onClick={() => {remove(lunchFoods.id,"l")
 
-const handleSubmit = (e: any) => {
-  e.preventDefault();
- 
-  //setFoodItem({name:"", calories:""}); 
-  //setFoodItem(parseInt(foodItem.calories))
-  setCurrCalories( currCalories - foodItem.calories);
-  console.log(foodItem);
-};
+}}>Remove</button>
+            {/* <button onClick={() => remove(foods.id)}>X</button> */}
+        </h1>
+      </div>
+    ))
+} 
 
-  const empty = (e:any) => {
-    e.preventDefault();
-    setFoodItem({name:"",calories:0});
-    console.log(foodItem);
+function displayDinnerItems(List:any) {
+  return dinnerItem.map((dinnerFoods)=> (
+
+    <div>
+      <h1>{dinnerFoods.name}  {dinnerFoods.calories} <button onClick={() => {remove(dinnerFoods.id,"d")
+
+}}>Remove</button>
+          {/* <button onClick={() => remove(foods.id)}>X</button> */}
+      </h1>
+    </div>
+  ))
+} 
+
+function displaySnackItems(List:any) {
+  return snackItem.map((snackFoods)=> (
+
+    <div>
+      <h1>{snackFoods.name}  {snackFoods.calories} <button onClick={() => {remove(snackFoods.id,"s")
+
+}}>Remove</button>
+          {/* <button onClick={() => remove(foods.id)}>X</button> */}
+      </h1>
+    </div>
+  ))
+} 
+
+  
+  const remove = (e:string,r:string) => {
+
+    if(r.localeCompare("b") === 0)
+    {
+      const newList = breakfastItem.filter(foodL => foodL.id != e);
+      setBreakfastItem(newList);
+      window.location.reload();
+      console.log('Removed from list ', breakfastItem);
+    }
+    else if(r.localeCompare("l") === 0)
+    {
+      const newList = lunchItem.filter(foodL => foodL.id != e);
+      setLunchItem(newList);
+      window.location.reload();
+      console.log('Removed from list ', lunchItem);
+    }
+    else if(r.localeCompare("d") === 0)
+    {
+      const newList = dinnerItem.filter(foodL => foodL.id != e);
+      setDinnerItem(newList);
+      window.location.reload();
+      console.log('Removed from list ', dinnerItem);
+    }
+   else if(r.localeCompare("s") === 0)
+    {
+      const newList = snackItem.filter(foodL => foodL.id != e);
+      setSnackItem(newList);
+      window.location.reload();
+      console.log('Removed from list ', snackItem);
+    }
+      
   }
 
-  const show =(e:any) => {
-    e.preventDefault();
-    console.log(foodItem)
+   const empty = (e:string) => {
+
+    if(e.localeCompare("b") === 0)
+    {
+      setBreakfastItem([{id:'', name:"",calories:0}])
+    }
+    else if(e.localeCompare("l") === 0)
+    {
+      setLunchItem([{id:'', name:"",calories:0}])
+    }
+    else if(e.localeCompare("d") === 0)
+    {
+      setLunchItem([{id:'', name:"",calories:0}])
+    }
+    else if(e.localeCompare("s") === 0)
+    {
+      setLunchItem([{id:'', name:"",calories:0}])
+    }
+     console.log(breakfastItem);
+   }
+
+  // const show =(e:any) => {
+  //   e.preventDefault();
+  //   console.log(foodItem)
+  // }
+
+  const displayForm = (e:String) => {
+    if(e.localeCompare("b") === 0)
+    {
+      setBreakfast(!breakfast);
+      setShowForm(!showForm);
+      setLunch(false);
+    }
+    else if(e.localeCompare("l") === 0)
+    {
+      setLunch(true);
+      setShowForm(!showForm);
+      setBreakfast(false);
+    }
+    else if(e.localeCompare("d") === 0)
+    {
+      setDinner(!dinner);
+      setShowForm(!showForm);
+      setBreakfast(false);
+      setLunch(false);
+    }
+    else if(e.localeCompare("s") === 0)
+    {
+      setSnack(!snack);
+      setShowForm(!showForm);
+      setBreakfast(false);
+      setLunch(false);
+      setDinner(false);
+    }
+    
   }
 
+useEffect(() => {
+  //Calories left 
+  const data = JSON.parse(
+    localStorage.getItem('calories') || '0');
+    //Food list
+  let foodData = JSON.parse(
+    localStorage.getItem('foodlist') || '{}')
+
+    //Initial calorie input 
+  let initCalo = JSON.parse(
+    localStorage.getItem('InitCalorie') || '0'
+  )
+
+  //Total amount of calories used
+  let totalCalories = JSON.parse(localStorage.getItem('usedUpCalorie') || '0');
+
+
+  setCurrCalories(data);
+  setBreakfastItem(foodData);
+  setInitCalorie(initCalo);
+  setUsedCalorie(totalCalories);
+  localStorage.clear();
+  // console.log('this is' , foodData);
+  // console.log('data',data);
+},[])
 
 useEffect(()=> {
-  localStorage.setItem('item', JSON.stringify(foodItem));
-})
+  //setCurrCalories(Number(localStorage.getItem('calories')));
+
+  localStorage.setItem('calories',
+                   JSON.stringify(currCalories));
+
+  },[currCalories])
+
+useEffect(() => {
+  localStorage.setItem('foodlist', JSON.stringify(breakfastItem));
+},[breakfastItem])
 
   return (
     <Background>
       <Title>  
           Track your calories
       </Title>
-
-      <h3>CALORIES</h3>
-
+      
       <CalorieButton onClick={inputCal}>
         Set calorie goals for the day
       </CalorieButton>
-      
-      <Calories> {currCalories}
-      </Calories>
+      <h3>CALORIES REMAINING</h3>
 
-      <form onSubmit={handleSubmit}>
+
+      <Calories>
+        {initCalorie} - {usedCalorie} = {currCalories}
+      </Calories>
+      
+
+      
+      {/* <form onSubmit={handleSubmit}>
       <label>Food item:
-      <input type="text" name ="name" value={foodItem.name} 
+      <input type="text" name ="foodName" defaultValue={''} 
         onChange={(e:any) => 
-              setFoodItem({...foodItem,name: e.target.value})}></input>
+              setFoodName(e.target.value)}></input>
       </label>
       <label>Calories:
-      <input type="text" name = "calories" 
-      value={foodItem.calories}
-      onChange={(e:any) => setFoodItem({...foodItem,
-      calories: Number(e.target.value)})}></input>
+      <input type="text" name = "foodCalorie" 
+      defaultValue={0}
+      onChange={(e:any) => setFoodCalorie(parseInt(
+      e.target.value))}></input>
       </label>
       <button type="submit">Enter food</button>
-      </form>
+      </form> */}
 
-      <button onClick={empty}>Clean list</button>
-      <button onClick={show}>Show list</button>
+      {/* <button onClick={empty}>Clean list</button>
+      <button onClick={show}>Show list</button> */}
+      
+        <h1>
+          Breakfast
+        </h1>
+        {displayBreakfastItems(breakfastItem)}
+        <button onClick={() => displayForm("b")}>ADD ITEM</button>
+        <button onClick={() => {empty("b")}}>CLEAR</button>
+        {showForm && breakfast && (
+          <form onSubmit={(e) => handleSubmit(e)}>
+          <label>Food item:
+          <input type="text" name ="foodName" defaultValue={''} 
+            onChange={(e:any) => 
+                  setFoodName(e.target.value)}></input>
+          </label>
+          <label>Calories:
+          <input type="text" name = "foodCalorie" 
+          //defaultValue={0}
+          onChange={(e:any) => setFoodCalorie(parseInt(
+          e.target.value))}></input>
+          </label>
+          <button type="submit" onClick={() => 
+            {changeCategory("b")}}>Enter food</button>
+          </form>
+
+      )}
+        <Line></Line>
+        <div>
+        <h1>
+          Lunch
+        </h1>
+        {displayLunchItems(lunchItem)}
+        <button onClick={() => displayForm("l")}>ADD ITEM</button>
+        <button onClick={() => {empty("l")}}>CLEAR</button>
+
+        {showForm && lunch && (
+          <form onSubmit={(e) => handleSubmit(e)}>
+          <label>Food item:
+          <input type="text" name ="foodName" //defaultValue={''} 
+            onChange={(e:any) => 
+                  setFoodName(e.target.value)}></input>
+          </label>
+          <label>Calories:
+          <input type="text" name = "foodCalorie" 
+          defaultValue={0}
+          onChange={(e:any) => setFoodCalorie(parseInt(
+          e.target.value))}></input>
+          </label>
+          <button type="submit" onClick={() => {changeCategory("l")}}>Enter food</button>
+          </form>)}
+          </div>
+        
+        <Line></Line>
+        <div>
+
+        
+        <h1>
+          Dinner
+        </h1>
+        {displayDinnerItems(dinnerItem)}
+        <button onClick={() => {displayForm("d")}}>ADD ITEM</button>
+        <button onClick={() => {empty("d")}}>CLEAR</button>
+        {showForm && dinner && (
+          <form onSubmit={(e) => handleSubmit(e)}>
+          <label>Food item:
+          <input type="text" name ="foodName" defaultValue={''} 
+            onChange={(e:any) => 
+                  setFoodName(e.target.value)}></input>
+          </label>
+          <label>Calories:
+          <input type="text" name = "foodCalorie" 
+          defaultValue={0}
+          onChange={(e:any) => setFoodCalorie(parseInt(
+          e.target.value))}></input>
+          </label>
+          <button type="submit" onClick={() => {changeCategory("d")}}>Enter food</button>
+          </form>)}
+        <Line></Line>
+        </div>
+        <p>
+          
+        <h1>
+          Snack
+        </h1>
+        {displaySnackItems(snackItem)}
+        <button onClick={() => {displayForm("s")}}>ADD ITEM</button>
+        <button onClick={() => {empty("s")}}>CLEAR</button>
+        {showForm && snack && (
+          <form onSubmit={(e) => handleSubmit(e)}>
+          <label>Food item:
+          <input type="text" name ="foodName" defaultValue={''} 
+            onChange={(e:any) => 
+                  setFoodName(e.target.value)}></input>
+          </label>
+          <label>Calories:
+          <input type="text" name = "foodCalorie" 
+          defaultValue={0}
+          onChange={(e:any) => setFoodCalorie(parseInt(
+          e.target.value))}></input>
+          </label>
+          <button type="submit" onClick={() => {changeCategory("s")}}>Enter food</button>
+          </form>)}
+        <Line></Line>
+
+        
+        
+        </p>
+      
       
     </Background>
   );
